322bf52b207512e9fc73712621a3add205eebd9e
Robert Wunderlich <robert.wunderlich@oracle.com>
2018-04-10 20:30:17 -0400

    Cleaned up unnecessary files

--- docker/build/apics_base/scratch/oracle/gitlocal/soa-apiplatform/stopEnv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DOMAIN_ROOT=/scratch/oracle/gitlocal/soa-apiplatform/out/domains
-
-$DOMAIN_ROOT/gateway1/bin/stopWebLogic.sh
-$DOMAIN_ROOT/drt/bin/stopWebLogic.sh
-
-exit
-
--- docker/build/ticketStatus/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export http_proxy=http://www-proxy.us.oracle.com:80
-export https_proxy=$http_proxy
-export HTTP_PROXY=$http_proxy
-export HTTPS_PROXY=$http_proxy
-
-pip install eve
-
